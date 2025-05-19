d5b492c59e3249483f80ac9546d97719b7742752
LucasMejer <lucas.mm@live.dk>
2025-05-19 18:40:01 +0200

    Burger Menu v1

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 let menuVariable = document.getElementById("menuUL");
-let logoFade = document.getElementById("logoFade");
+let burgerMenuVariable = document.getElementById("burgerMenuUL");
 
 var path = window.location.pathname;
 var page = path.split("/").pop();
@@ -23,11 +23,35 @@ const menuItems = [
 for(let i = 0; i < menuItems.length; i++){
 
     if(page == menuItems[i].link){
-        menuVariable.innerHTML += "<li class=activeNav>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</li>";
+        menuVariable.innerHTML += "<li class=activeNav>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</li>"
+        burgerMenuVariable.innerHTML += "<li class=activeNav>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</li>";
     }
 
     else{
         menuVariable.innerHTML += "<li>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</li>";
+        burgerMenuVariable.innerHTML += "<li>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</li>"
     }
 
+}
+
+/* Burger Menu */
+
+function burgerMenuFunction() {
+  var x = document.getElementById("burgerMenuUL");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var jsMedia = window.matchMedia("(max-width: 768px)")
+
+jsMedia.addEventListener("change", function(){
+    jsMediaFunction(jsMedia);
+})
+
+function jsMediaFunction(){
+    var x = document.getElementById("burgerMenuUL");
+    x.style.display = "none";
 }
